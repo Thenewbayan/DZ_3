d3b6ab13b9bf8b945c3055aed0af4e7a13f8f985
Thenewbayan <chemspirit.trade@gmail.com>
2023-02-19 21:14:27 +0300

    koordinaty gotovy

---
+++ cuby/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+Console.WriteLine("Найдем таблицу кубов для любых чисел (от 1 до вашего числа), введите число");
+int Number=Convert.ToInt32(Console.ReadLine());
+int count=1;
+double cube;
+while (count<=Number)
+{
+    cube=Math.Pow(count,3);
+    Console.Write(cube+" "+"\n");
+    count=count+1;
+}
+Console.WriteLine($"Мы вывели значения всех кубов чисел от 1 до числа {Number}");
---
+++ koordinaty/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+// A (3,6,8); B (2,1,-7), -> 15.84
+
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+Console.WriteLine("Найдем расстояние между точками в 3д системе координат");
+Console.WriteLine("Введите координаты точки А в формате x,y,z после каждого числа нажимайте enter");
+int XA=Convert.ToInt32(Console.ReadLine());
+int YA=Convert.ToInt32(Console.ReadLine());
+int ZA=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координаты точки B в формате x,y,z после каждого числа нажимайте enter");
+int XB=Convert.ToInt32(Console.ReadLine());
+int YB=Convert.ToInt32(Console.ReadLine());
+int ZB=Convert.ToInt32(Console.ReadLine());
+var CORD=Math.Sqrt((Math.Pow(XA-XB, 2)+Math.Pow(YA-YB, 2)+Math.Pow(ZA-ZB, 2)));
+Console.WriteLine(Math.Round(CORD,2));
