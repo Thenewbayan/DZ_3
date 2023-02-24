fba6a51704b89da97e1507bf2cd50d972ad2902c
Thenewbayan <chemspirit.trade@gmail.com>
2023-02-24 19:12:04 +0300

    interes ok

--- interesnoe_chislo/Program.cs
+++ interesnoe_chislo/Program.cs
@@ -2,35 +2,32 @@
 //создать массив из 10 случайных интересных чисел в диапазоне от 10 до 1000(я так понял что исключая 1000)
 //начнемссссс
 Console.WriteLine("Число считаем интересным, если произведение его цифр кратно их сумме");
-int [] InterestNumbers={};
+int [] InterestNumbers=new int [10];
 int index=0;//массив интересных чисел
 int count=0;
-while(index<10)// пока не заполним массив
+while(index<InterestNumbers.Length)// пока не заполним массив
 {
-//int number=new Random().Next(10,1000);//генерим случайное число в диапазоне
-int number =22;
-//находим его цифры по отдельности
-//Console.WriteLine(number);
+int number=new Random().Next(10,1000);//генерим случайное число в диапазоне
 int deegre=1;//стеень десятки для определения чисел
 int digit=1;//разрядность числа
-while(number/Math.Pow(10,deegre)!=0)
+while(number/(int)(Math.Pow(10,deegre))!=0)
 {
     digit=digit+1;
     deegre=deegre+1;
-    Console.WriteLine(digit);
-    Console.WriteLine(deegre);
+    
 }
+
 if (digit==2)//для 2 разрядных чисел
 {//числа а в с справо налево
     int numberA=Convert.ToInt32(number%10);
-    int numberB=Convert.ToInt32((number/Math.Pow(10, 1))%10);//нашли цифры для разрядов
+    int numberB=Convert.ToInt32((number/(int)(Math.Pow(10, 1)))%10);//нашли цифры для разрядов
     if((numberA*numberB)%(numberA+numberB)==0)//если совпадает то записываем в массив
     {
         InterestNumbers[index]=number;
-        Console.WriteLine(count);
+       
         count++;
         index++;
-        Console.WriteLine(number);
+       
         
     }
     else //если нет то счетчик ++
@@ -41,15 +38,13 @@ if (digit==2)//для 2 разрядных чисел
 if (digit==3)//для трехразрядных чисел
 {
    int numberA=Convert.ToInt32(number%10);
-   int numberB=Convert.ToInt32((number/Math.Pow(10, 1))%10); 
-   int numberC=Convert.ToInt32((number/Math.Pow(10, 2))%10);
+   int numberB=Convert.ToInt32((number/(int)(Math.Pow(10, 1)))%10); 
+   int numberC=Convert.ToInt32((number/(int)(Math.Pow(10, 2)))%10);
    if ((numberA*numberB*numberC)%(numberA+numberB+numberC)==0)//то же самое, находим разряды, если да, то в массив
    {
-    InterestNumbers[index]=number;
-        //Console.WriteLine(count);
+        InterestNumbers[index]=number;
         count++;
         index++;
-       // Console.WriteLine(number);
    }
    else
    {
@@ -57,6 +52,8 @@ if (digit==3)//для трехразрядных чисел
    }
 }
 }
-Console.WriteLine(InterestNumbers);
-double KPI=(index/count)*100;
-Console.WriteLine(Math.Round(KPI, 3));
+
+Console.WriteLine(String.Join(";", InterestNumbers)); 
+
+double KPI=((index/count)*100);
+Console.WriteLine((Math.Round(KPI, 10)+"%"));
